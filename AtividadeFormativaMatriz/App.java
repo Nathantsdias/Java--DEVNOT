96f82a09d97a87677e0592776966b62902f9069e
nathantsdias <nathantsdias@gmail.com>
2023-05-19 20:48:15 -0300

    19.05

---
+++ AtividadeFormativaMatriz/App.java
@@ -0,0 +1,8 @@
+package AtividadeFormativaMatriz;
+
+public class App {
+    public static void main(String[] args) {
+        AtividadeFormativa obj1 = new AtividadeFormativa();
+        obj1.Exercicio1();
+    }
+}
---
+++ AtividadeFormativaMatriz/AtividadeFormativa.java
@@ -0,0 +1,59 @@
+package AtividadeFormativaMatriz;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class AtividadeFormativa {
+    Scanner sc = new Scanner(System.in);
+    Random rd = new Random();
+
+    public void Exercicio1() {
+        System.out.println("Insira o numero de Linhas: ");
+        int linha = sc.nextInt();
+        System.out.println("Insira o numero de Colunas: "); 
+        int coluna = sc.nextInt();
+        int matriz[][] = new int [linha][coluna];
+        for (int i = 0; i < linha; i++) {
+            for (int j = 0; j < coluna; j++) {
+                matriz[i][j] = rd.nextInt(10);
+            }
+        }
+
+        System.out.println("Primeira Matriz ");
+        imprimirMatriz(matriz);
+
+
+        for (int i = 0; i < linha; i++) {
+            for (int j = 0; j < coluna; j++) {
+                if (i > j) {
+                    matriz[i][j] = 1;
+                } else if (i == j) {
+                    matriz[i][j] = 0;
+                } else {
+                    matriz[i][j] = 2;
+                }
+            }
+        }
+
+       
+        System.out.println("Matriz Substituida: ");
+        imprimirMatriz(matriz);
+    }
+
+    private void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// int vetor[10], indice;
+// for (indice=0; indice<10; indice++)
+// {
+// printf("\nVetor[%d]: ",indice);
+// scanf("%d",vetor[indice]);
+// }
+// }
+// }
